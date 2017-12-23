3c2ccde825b05c5b8db076732d6483dfb93bf225
Kirill Starikov <kstarikov@alertlogic.com>
2017-12-23 11:39:59 +0000

    getting fn-fx as a dependency from Github

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
+                 ^{:voom {:repo "https://github.com/halgari/fn-fx" :branch "master"}}
                  [org.clojure/clojure "1.8.0"]
                  [org.fxmisc.richtext/richtextfx "0.8.1"]
-                 [halgari/fn-fx "0.4.0"]
                  ]
   :main ^:skip-aot test-fn-fx.core
   :target-path "target/%s"
