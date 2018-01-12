79758dede89907f4eebc4a960a4e1de69f02b1d6
Kirill Starikov <kstarikov@alertlogic.com>
2018-01-12 21:48:22 +0000

    moving to clojure 1.9 and the local version of `fn-fx`

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"local" ~(str (.toURI (java.io.File. "/Users/kirillstarikov/.m2/repository")))}
   :dependencies [
-                 ^{:voom {:repo "https://github.com/halgari/fn-fx" :branch "master"}}
-                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.9.0"]
                  [org.fxmisc.richtext/richtextfx "0.8.1"]
+                 [org.fxmisc.flowless/flowless "0.5.2"]
+                 [halgari/fn-fx "0.5.0-SNAPSHOT"]
                  ]
   :main ^:skip-aot test-fn-fx.core
   :target-path "target/%s"
