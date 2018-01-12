afbaf41733f0d8956416600b6dd473148e22a113
Kirill Starikov <kstarikov@alertlogic.com>
2018-01-12 21:49:21 +0000

    primitive text component implemented with CodeArea

--- src/test_fn_fx/core.clj
+++ src/test_fn_fx/core.clj
@@ -1,74 +1,64 @@
 (ns test-fn-fx.core
   (:require [fn-fx.fx-dom :as dom]
+            [fn-fx.render-core :as render-core]
             [fn-fx.diff :refer [component defui render should-update?]]
-            [fn-fx.controls :as ui]))
+            [fn-fx.controls :as ui])
+  (:gen-class))
 
 (import 'org.fxmisc.richtext.CodeArea)
+(import 'org.fxmisc.flowless.VirtualizedScrollPane)
 
 (defmacro ca [& {:as props}]
   (fn-fx.render-core/component-impl :org.fxmisc.richtext.CodeArea props #{}))
 
-(comment 
-  
- (let [prop "minHeight"] (fn-fx.util/kabob->class (clojure.string/replace (name prop) "?" "")))
+(defmacro vsp [& {:as props}]
+  (fn-fx.render-core/component-impl :org.fxmisc.flowless.VirtualizedScrollPane props #{}))
 
- (let [prop "minHeight"] (str "set" (fn-fx.util/kabob->class (clojure.string/replace (name prop) "?" ""))))
+(defui MainWindow
+  (render [this args]
+          (ui/grid-pane
+            :alignment :center
+            :hgap 10
+            :vgap 10
+            :padding (ui/insets
+                       :bottom 25
+                       :left 25
+                       :right 25
+                       :top 25)
+            :children [(ca
+                          :minHeight 400
+                          :minWidth 400
+                          :editable true
+                          )
+                        ]
+            )))
 
-)
-
-;; The main login window component, notice the authed? parameter, this defines a function
-;; we can use to construct these ui components, named "login-form"
-(defui LoginWindow
-       (render [this {:keys [authed?]}]
-               (ui/grid-pane
-                 :alignment :center
-                 :hgap 10
-                 :vgap 10
-                 :padding (ui/insets
-                            :bottom 25
-                            :left 25
-                            :right 25
-                            :top 25)
-                 :children [(ca
-                              :minHeight 300
-                              :editable true
-                              )
-                            (ui/text-flow
-                              :children
-                              [(ui/text
-                                 :text "Welcome"
-                                 :font (ui/font
-                                         :family "Tahoma"
-                                         :weight :normal
-                                         :size 20))
-                               (ui/text
-                                 :text "Motherfucker"
-                                 :font (ui/font
-                                         :family "Tahoma"
-                                         :weight :normal
-                                         :size 20))])
-                            ])))
-
-;; Wrap our login form in a stage/scene, and create a "stage" function
 (defui Stage
        (render [this args]
                (ui/stage
-                 :title "JavaFX Welcome"
+                 :title "Text"
                  :shown true
                  :scene (ui/scene
-                          :root (login-window args)))))
+                          :root (main-window args)))))
 
 (defn -main []
-  (let [;; Data State holds the business logic of our app
-        data-state (atom {:authed? false})
-
-        ;; handler-fn handles events from the ui and updates the data state
-        handler-fn (fn [{:keys [event] :as all-data}]
+        (def handler-fn (fn [{:keys [event] :as all-data}]
                      (println "UI Event" event all-data)
-                     )
+                     ))
 
         ;; ui-state holds the most recent state of the ui
-        ui-state (agent (dom/app (stage @data-state) handler-fn))]
+        (def ui-state (agent (dom/app (stage [] ) handler-fn)))
+  
+  )
 
-    ;; Every time the data-state changes, queue up an update of the UI
-    ))
+(comment
+
+        (def handler-fn (fn [{:keys [event] :as all-data}]
+                     (println "UI Event" event all-data)
+                     ))
+
+        (def ui-state (agent (dom/app (stage [] ) handler-fn)))
+
+      (render-core/get-property (first (reduce (fn [ comp field] ( render-core/get-property comp field )) @(:root @ui-state) [:scene :root :children])) :text )
+
+    )
